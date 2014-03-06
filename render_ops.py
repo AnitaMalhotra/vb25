ae93b41e4d68737ed4eb7c2e547fac914c14d594
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-03-06 17:39:24 +0200

    fix: update for using submodule

--- render_ops.py
+++ render_ops.py
@@ -45,10 +45,12 @@ import vb25.render
 import vb25.proxy
 
 from vb25.lib     import VRaySocket
-from vb25.lib     import VRayProxy
-from vb25.lib     import GetMaterialsNames, GetXMLMaterialsNames
 from vb25.utils   import *
 from vb25.plugins import *
+
+from vb25.vray_tools                 import VRayProxy
+from vb25.vray_tools.VRaySceneParser import GetMaterialsNames
+from vb25.vray_tools.VrmatParser     import GetXMLMaterialsNames
 
 
 VRAYBLENDER_MENU_ITEM= "V-Ray"
